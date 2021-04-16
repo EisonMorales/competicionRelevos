9472dbf23d66a1a0552c5461c432879450ab23f3
EisonMorales <eandreimorales@ucundinamarca.edu.co>
2021-04-15 21:52:55 -0500

    creacion de repositorio

---
+++ src/main/java/co/edu/ucundinamarca/controlador/Aplicacion.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package co.edu.ucundinamarca.controlador;
+
+import co.edu.ucundinamarca.vista.Principal;
+
+/**
+ * Clase encargada de realizar la ejecución del programa.
+ * 
+ * @author Eison Andrei Morales Pardo
+ * @author Juan Camilo Páez Beltran 
+ * @since 1.0
+ * @version 1.0.0
+ */
+public class Aplicacion {
+    /*public Aplicacion() {
+        Principal c = new Principal();
+        c.cualquiera();
+    }
+    public static void main(String[] args) {
+        new Aplicacion();
+    }*/
+}
